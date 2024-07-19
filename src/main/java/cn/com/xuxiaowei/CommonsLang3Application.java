fdd5a5d7bef89e22bc33002d17d4c00c6be0251d
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2024-07-19 09:43:06 +0800

    init

---
+++ src/main/java/cn/com/xuxiaowei/CommonsLang3Application.java
@@ -0,0 +1,13 @@
+package cn.com.xuxiaowei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonsLang3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonsLang3Application.class, args);
+	}
+
+}
---
+++ src/test/java/cn/com/xuxiaowei/CommonsLang3ApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.com.xuxiaowei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonsLang3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
