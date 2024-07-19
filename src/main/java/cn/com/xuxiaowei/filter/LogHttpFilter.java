f950da93af59fba97014e8a523408416f909a293
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2024-07-19 10:02:18 +0800

    add LogHttpFilter

---
+++ src/main/java/cn/com/xuxiaowei/filter/LogHttpFilter.java
@@ -0,0 +1,44 @@
+package cn.com.xuxiaowei.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+/**
+ * 日志 过滤器
+ *
+ * @author xuxiaowei
+ * @since 0.0.1
+ */
+@Slf4j
+@Setter
+@Component
+public class LogHttpFilter extends HttpFilter implements Ordered {
+
+    public static final int ORDERED = Ordered.HIGHEST_PRECEDENCE;
+
+    private int order = ORDERED;
+
+    @Override
+    public int getOrder() {
+        return order;
+    }
+
+    @Override
+    protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+        log.info(String.valueOf(LocalDateTime.now()));
+
+        super.doFilter(request, response, chain);
+    }
+
+}
